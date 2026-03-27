de9f4b02b3881b01a19d6dc7040c77ddf7cc27c7
aluno <e023aluno@outlook.com>
2026-03-27 17:46:19 -0300

    Zedamanga

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/', (req, res)=>{
     res.send("Servidor rodando na nuvem 🚀 ")
+});
+
+app.get('/Teste', (req, res)=>{
+    res.send("Teste")
 })
 
 const PORT = process.env.PORT || 3000;
